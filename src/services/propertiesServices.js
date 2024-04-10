91de9ef0f156092c975fb09b8ef788cdf8814e4f
FlorPM <florpm1991@gmail.com>
2024-04-10 17:36:29 +0200

    feat: adds get all properties service

--- src/services/propertiesServices.js
+++ src/services/propertiesServices.js
@@ -1,1 +1,12 @@
-// Archivo para los fetch de properties a la API
+const APIUrl = import.meta.env.VITE_API_URL
+
+// Ver todas las propiedades
+const getAllPropertiesService = async () => {
+    const res = await fetch(`${APIUrl}/properties`)
+
+    const body = await res.json()
+
+    return body
+}
+
+export { getAllPropertiesService }
