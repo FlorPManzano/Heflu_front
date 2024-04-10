d21873e7dcfb803920af53027208a5dbe474f97a
FlorPM <florpm1991@gmail.com>
2024-04-10 17:35:54 +0200

    feat: adds properties hook

--- src/hooks/useProperties.js
+++ src/hooks/useProperties.js
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react"
+import { getAllPropertiesService } from "../services/propertiesServices"
+
 export const useProperties = () => {
-    return
+    const [properties, setProperties] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchProperties = async () => {
+            try {
+                setLoading(true)
+                const body = await getAllPropertiesService()
+                setProperties(body.data)
+            } catch (err) {
+                console.log(err.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchProperties()
+    }, [])
+
+    return {
+        properties,
+        loading,
+    }
 }
