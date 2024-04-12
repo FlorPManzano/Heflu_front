7162353781ba52561d8a7db0232a9656ca4df9c1
FlorPM <florpm1991@gmail.com>
2024-04-12 17:12:09 +0200

    chore: installs and config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1e1e1e",
+            },
+        },
+    },
+    plugins: [],
+}
