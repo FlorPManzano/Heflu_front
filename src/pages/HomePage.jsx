4b44f4227a7fc3e1891e1c8aad387501e2fa86c9
FlorPM <florpm1991@gmail.com>
2024-04-12 19:55:55 +0200

    refactor: composes login and register modals

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,48 +9,8 @@ import Hero from "../components/Hero"
 const APIUrl = import.meta.env.VITE_API_URL
 
 export default function HomePage() {
-    const { authRegister, authLogin } = useAuth()
     const { properties } = useProperties()
-
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [bio, setBio] = useState("")
-    const [avatar, setAvatar] = useState(null)
-    const fileInputRef = useRef()
-
-    const [loginEmail, setLoginEmail] = useState("")
-    const [loginPassword, setLoginPassword] = useState("")
-
     const navigate = useNavigate()
-
-    const registerSubmit = async (e) => {
-        e.preventDefault()
-        const image = fileInputRef.current?.files[0]
-
-        const formData = new FormData()
-        formData.set("name", name)
-        formData.set("email", email)
-        formData.set("password", password)
-        formData.set("bio", bio)
-        formData.set("avatar", image)
-
-        try {
-            await authRegister(formData)
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
-
-    const logInSubmit = async (e) => {
-        e.preventDefault()
-        console.log(loginEmail, loginPassword)
-        try {
-            await authLogin({ email: loginEmail, password: loginPassword })
-        } catch (error) {
-            console.log(error.message)
-        }
-    }
 
     const handleCardClick = async (e, key) => {
         e.preventDefault()
@@ -60,81 +20,6 @@ export default function HomePage() {
     return (
         <>
             <Hero />
-            {/* Formulario de registro */}
-            <form action="" onSubmit={registerSubmit}>
-                <h2>Registro</h2>
-                <div>
-                    <input
-                        id="name"
-                        type="text"
-                        required
-                        value={name && name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                    <label htmlFor="name">Nombre</label>
-                </div>
-                <div>
-                    <input
-                        id="email"
-                        type="email"
-                        required
-                        value={email && email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <label htmlFor="email">Email</label>
-                </div>
-                <div>
-                    <input
-                        id="password"
-                        type="password"
-                        required
-                        value={password && password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <label htmlFor="password">Contraseña</label>
-                </div>
-                <div>
-                    <input
-                        id="bio"
-                        type="text"
-                        required
-                        value={bio && bio}
-                        onChange={(e) => setBio(e.target.value)}
-                    />
-                    <label htmlFor="bio">Biografia</label>
-                </div>
-                <div>
-                    <input
-                        id="avatar"
-                        type="file"
-                        required
-                        ref={fileInputRef}
-                        onChange={(e) => setAvatar(e.target.value)}
-                    />
-                    <label htmlFor="avatar">Avatar</label>
-                </div>
-
-                <button type="submit">Registro</button>
-            </form>
-            <h2>Login</h2>
-            {/* Formulario de login */}
-            <form action="" onSubmit={logInSubmit}>
-                <div>
-                    <input
-                        type="email"
-                        onChange={(e) => setLoginEmail(e.target.value)}
-                    />
-                    <label htmlFor="">Email</label>
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        onChange={(e) => setLoginPassword(e.target.value)}
-                    />
-                    <label htmlFor="">Contraseña</label>
-                </div>
-                <button type="submit">Inicia sesión</button>
-            </form>
             {/* Listado de propiedades */}
             <section>
                 <div>
