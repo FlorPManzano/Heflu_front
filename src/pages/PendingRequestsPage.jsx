8f821e206683ab81d413813f0f13dd977963cd6a
FlorPM <florpm1991@gmail.com>
2024-04-21 12:38:29 +0200

    feat: adds pending requests to page

--- src/pages/PendingRequestsPage.jsx
+++ src/pages/PendingRequestsPage.jsx
@@ -1,3 +1,114 @@
+import { useBookings } from "../hooks/useBookings"
+
+const APIUrl = import.meta.env.VITE_API_URL
+
 export default function PendingRequestsPage() {
-    return <h1>PendingRequestsPage</h1>
+    const { bookings } = useBookings()
+
+    function formatDate(dateString) {
+        const weekDays = [
+            "Domingo",
+            "Lunes",
+            "Martes",
+            "Miércoles",
+            "Jueves",
+            "Viernes",
+            "Sábado",
+        ]
+        const months = [
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre",
+        ]
+
+        const date = new Date(dateString)
+        const weekDay = weekDays[date.getUTCDay()]
+        const day = date.getUTCDate()
+        const month = months[date.getUTCMonth()]
+        const year = date.getUTCFullYear()
+
+        return `${weekDay} ${day} de ${month} de ${year}`
+    }
+
+    return (
+        <section className="mt-32 mb-10 mx-6 text-primary">
+            <h2 className="text-xl text-violet-700 font-bold border-b border-primary/80 pb-2 mb-4 tracking-wide capitalize">
+                Mis solicitudes pendientes
+            </h2>
+            <div className=" flex flex-col gap-y-4 items-center">
+                {bookings && bookings.length > 0 ? (
+                    bookings.map((booking) => (
+                        <article
+                            key={booking.id}
+                            className="flex gap-6 p-4 border border-primary/20 rounded-2xl shadow-md max-w-[760px]"
+                        >
+                            <img
+                                src={`${APIUrl}/${booking.images[0]}`}
+                                alt="Property"
+                                className="w-44 h-44 object-cover rounded-2xl"
+                            />
+                            <div>
+                                <h4 className="font-semibold capitalize text-lg pb-2">
+                                    Solicitud de reserva para "
+                                    {booking.property}"
+                                </h4>
+                                <p className="text-sm pb-3">
+                                    Tienes una solicitud de reserva para el
+                                    inmueble situado en{" "}
+                                    <span className="font-semibold">
+                                        {booking.location}
+                                    </span>{" "}
+                                    a nombre de{" "}
+                                    <span className="font-semibold">
+                                        {booking.tenant}.
+                                    </span>{" "}
+                                    La información de la reserva es la
+                                    siguiente:
+                                </p>
+                                <ul className="list-disc ml-5 text-sm">
+                                    <li>
+                                        <span className="font-semibold">
+                                            Fecha de entrada:
+                                        </span>{" "}
+                                        {formatDate(booking.starting_date)}
+                                    </li>
+                                    <li>
+                                        <span className="font-semibold">
+                                            Fecha de salida:
+                                        </span>{" "}
+                                        {formatDate(booking.ending_date)}
+                                    </li>
+                                    <li>
+                                        <span className="font-semibold">
+                                            Precio total:
+                                        </span>{" "}
+                                        {booking.total_price} €
+                                    </li>
+                                </ul>
+                                <div className="flex justify-end items-end">
+                                    <button className="text-violet-700 font-medium text-sm rounded-lg mr-4 hover:underline transition duration-300 ease-in-out capitalize">
+                                        Cancelar
+                                    </button>
+                                    <button className="bg-violet-700 border border-violet-700 font-normal text-white px-2 py-1 text-sm rounded-lg hover:shadow-lg transition-shadow duration-300 ease-in-out">
+                                        Aceptar
+                                    </button>
+                                </div>
+                            </div>
+                        </article>
+                    ))
+                ) : (
+                    <p>No hay reservas pendientes.</p>
+                )}
+            </div>
+        </section>
+    )
 }
