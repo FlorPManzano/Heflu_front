06704976dc718ad45d5d7005fbea0c7010e83328
HectornsGit <hector.novoa.segade@gmail.com>
2024-04-22 19:11:40 +0200

    chore: Installs the react-toastify dependency and adds the ToastContainer to main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from "./App.jsx"
 import { BrowserRouter } from "react-router-dom"
 import { AuthProvider } from "./contexts/AuthContext.jsx"
 import "./index.css"
-
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
                 <App />
+                <ToastContainer />
             </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
