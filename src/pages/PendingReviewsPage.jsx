a0b3e85f26ca133a56e4cc29c7342182f4ab7157
FlorPM <florpm1991@gmail.com>
2024-04-24 10:57:07 +0200

    feat: adds reviews form

--- src/pages/PendingReviewsPage.jsx
+++ src/pages/PendingReviewsPage.jsx
@@ -6,17 +6,26 @@ import Rating from "@mui/material/Rating"
 const APIUrl = import.meta.env.VITE_API_URL
 export default function PendingReviewsPage() {
     const { reviews, addReview } = useReviews()
-    const [comment, setComment] = useState("")
-    const [rating, setRating] = useState(0)
+    const [comment, setComment] = useState({})
+    const [rating, setRating] = useState({})
     const { authUser } = useAuth()
-
-    console.log("reviews", reviews)
 
     const revType = (ownerId) => {
         if (authUser.id === ownerId) {
             return "inquilino"
         } else {
             return "casero"
+        }
+    }
+
+    const handleSubmit = async (e, key) => {
+        e.preventDefault()
+        try {
+            await addReview(key, rating[key], comment[key])
+            setComment({ ...comment, [key]: "" })
+            setRating({ ...rating, [key]: 0 })
+        } catch (error) {
+            console.log(error.message)
         }
     }
 
@@ -35,34 +44,47 @@ export default function PendingReviewsPage() {
                                     alt="Property"
                                     className="w-56 h-56 object-cover rounded-2xl"
                                 />
-                                <section className="flex flex-col gap-y-4 w-full justify-between">
+                                <section className="flex flex-col w-full justify-between">
                                     <h4 className="font-semibold capitalize text-md">
                                         Valoración al {revType(review.owner_id)}{" "}
                                         en "{review.title}"
                                     </h4>
-                                    <textarea
-                                        className="bg-transparent text-primary outline outline-0 focus:outline-0 placeholder-primary transition-all placeholder-shown:border placeholder-shown:border-primary/50 border focus:border-1 text-sm px-3 py-2.5 rounded-[10px] border-primary focus:border-violet-700 w-full"
-                                        id="comment"
-                                        type="text"
-                                        placeholder="Deja aquí tu comentario..."
-                                        required
-                                        value={comment && comment}
-                                        onChange={(e) =>
-                                            setComment(e.target.value)
+                                    <form
+                                        action=""
+                                        onSubmit={(e) =>
+                                            handleSubmit(e, review.id)
                                         }
-                                    />
-                                    <Rating
-                                        name="simple-controlled"
-                                        value={rating}
-                                        onChange={(e, newValue) => {
-                                            setRating(newValue)
-                                        }}
-                                    />
-                                    <div className="flex flex-row justify-end items-end">
-                                        <button className="bg-violet-700 border border-violet-700 font-normal text-white px-2 py-1 text-sm rounded-lg hover:shadow-lg transition-shadow duration-300 ease-in-out w-20">
-                                            Enviar
-                                        </button>
-                                    </div>
+                                    >
+                                        <textarea
+                                            className="bg-transparent text-primary outline outline-0 focus:outline-0 placeholder-primary transition-all placeholder-shown:border placeholder-shown:border-primary/50 border focus:border-1 text-sm px-3 py-2.5 rounded-[10px] border-primary focus:border-violet-700 w-full"
+                                            id="comment"
+                                            type="text"
+                                            placeholder="Deja aquí tu comentario..."
+                                            required
+                                            value={comment[review.id] || ""}
+                                            onChange={(e) =>
+                                                setComment({
+                                                    ...comment,
+                                                    [review.id]: e.target.value,
+                                                })
+                                            }
+                                        />
+                                        <Rating
+                                            name="simple-controlled"
+                                            value={rating[review.id] || 0}
+                                            onChange={(e, newValue) => {
+                                                setRating({
+                                                    ...rating,
+                                                    [review.id]: newValue,
+                                                })
+                                            }}
+                                        />
+                                        <div className="flex flex-row justify-end items-end">
+                                            <button className="bg-violet-700 border border-violet-700 font-normal text-white px-2 py-1 text-sm rounded-lg hover:shadow-lg transition-shadow duration-300 ease-in-out w-20">
+                                                Enviar
+                                            </button>
+                                        </div>
+                                    </form>
                                 </section>
                             </article>
                         </li>
