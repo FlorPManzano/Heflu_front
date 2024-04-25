d3712337328070413c120cbccdff614dc1d495e3
FlorPM <florpm1991@gmail.com>
2024-04-25 12:43:18 +0200

    feat: adds function to filter properties by user

--- src/hooks/useProperties.js
+++ src/hooks/useProperties.js
@@ -7,8 +7,9 @@ import useAuth from "./useAuth"
 
 export const useProperties = () => {
     const [properties, setProperties] = useState([])
+    const [userProperties, setUserProperties] = useState([])
     const [loading, setLoading] = useState(false)
-    const { authToken } = useAuth()
+    const { authToken, authUser } = useAuth()
 
     useEffect(() => {
         const fetchProperties = async () => {
@@ -23,6 +24,7 @@ export const useProperties = () => {
             }
         }
         fetchProperties()
+        if (authToken) fetchUserProperties()
     }, [])
 
     // Función para añadir propiedades
@@ -37,9 +39,28 @@ export const useProperties = () => {
         }
     }
 
+    // Función para filtrar propiedades por usuario
+
+    const fetchUserProperties = async () => {
+        try {
+            setLoading(true)
+            const body = await getAllPropertiesService()
+            const filteredProperties = body.data.filter((property) => {
+                return property.owner_id === authUser.id
+            })
+            setUserProperties(filteredProperties)
+        } catch (err) {
+            console.log(err.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return {
         properties,
         loading,
         addProperty,
+        userProperties,
+        fetchUserProperties,
     }
 }
