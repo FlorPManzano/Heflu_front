1441ea3fd7fdda6cf6e4ab423a90766e52fc83cc
HectornsGit <hector.novoa.segade@gmail.com>
2024-05-26 12:37:19 +0200

    refactor: changes the onClick function to a Link component

--- src/components/ListPropertiesCards.jsx
+++ src/components/ListPropertiesCards.jsx
@@ -1,22 +1,15 @@
 import PropertyCard from "./PropertyCard"
-import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const ListPropertiesCards = ({ properties }) => {
-    const navigate = useNavigate()
-
-    const handleCardClick = async (e, key) => {
-        e.preventDefault()
-        navigate(`/properties/${key}`)
-    }
     return (
         <ul className="flex flex-wrap justify-start gap-4 my-10">
             {properties.length > 0 &&
                 properties.map((property) => (
-                    <li
-                        key={property.id}
-                        onClick={(event) => handleCardClick(event, property.id)}
-                    >
-                        <PropertyCard property={property} />
+                    <li key={property.id}>
+                        <Link to={`/properties/${property.id}`}>
+                            <PropertyCard property={property} />
+                        </Link>
                     </li>
                 ))}
         </ul>
