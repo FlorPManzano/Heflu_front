965d534e6d49688bad8877e0d82c9078029c7f32
HectornsGit <hector.novoa.segade@gmail.com>
2024-06-04 18:03:13 +0200

    refactor: creates a file to encapsulate toast sending

---
+++ src/utils/sendToast.js
@@ -0,0 +1,28 @@
+import { toast, Bounce } from "react-toastify"
+export function sendErrorToast(body) {
+    toast.error(body.message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+    })
+}
+
+export function sendSuccessToast(body) {
+    toast(body.message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+    })
+}
